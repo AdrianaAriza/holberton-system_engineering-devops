18832a568bbb560572f95f743e5f0e23d22655c1
AdrianaAriza <1296@holbertonschool.com>
2020-04-04 00:44:48 -0500

    [FIx]

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,8 +3,8 @@ exec { 'http-header':
   command  => 'sudo apt-get -y upgrade &&
   	       sudo apt-get -y update &&
   	       sudo apt-get -y install nginx &&
-	       n3="sendfile on;\n\tadd_header X-Served-By $HOSTNAME;"
-	       sudo sed -i "s/sendfile on;/$n3/" /etc/nginx/nginx.conf &&
+	       n_s3="sendfile on;\n\tadd_header X-Served-By $HOSTNAME;" &&
+	       sudo sed -i "s/sendfile on;/$n_s3/" /etc/nginx/nginx.conf &&
 	       sudo service nginx start',
   provider => shell,
 }
