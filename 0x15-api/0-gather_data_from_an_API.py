5406be086c01f6ee683d443a8ef4d479b6e0a599
AdrianaAriza <1296@holbertonschool.com>
2020-05-04 19:38:53 -0500

    [Upload]: Task0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""""""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    reqs = requests.get(url + "todos", params={'userId': sys.argv[1]}).json()
+    x = len(reqs)
+    d_task = []
+    for req in reqs:
+        if req['completed'] is True:
+            d_task.append(req['title'])
+    reqs = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    print('Employee {} is done with tasks {}/{}'.format(reqs.get('name'), len(d_task), x))
+    for task in d_task:
+        print('\t' + task)
