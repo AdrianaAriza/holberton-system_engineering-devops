1c13a37bf7eb50e52782d411927741fbe4d6e30d
AdrianaAriza <1296@holbertonschool.com>
2020-05-04 20:23:43 -0500

    [Fix]: permision

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,14 +3,14 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    reqs = requests.get(url + "todos", params={'userId': sys.argv[1]}).json()
+    url = "https://jsonplaceholder.typicode.com/users"
+    reqs = requests.get('{}/{}/todos'.format(url, sys.argv[1])).json()
     x = len(reqs)
     d_task = []
     for req in reqs:
         if req['completed'] is True:
             d_task.append('\t {}'.format(req['title']))
-    reqs = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    reqs = requests.get(url + "/{}".format(sys.argv[1])).json()
     print('Employee {} is done with tasks({}/{}):'.format(reqs.get('name'),
           len(d_task), x))
     for task in d_task:
