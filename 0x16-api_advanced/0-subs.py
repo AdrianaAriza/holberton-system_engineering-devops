80abde61fc41ef43a2feb0755a7a9afef63ee36e
AdrianaAriza <1296@holbertonschool.com>
2020-05-07 18:46:15 -0500

    [Fix]: task0-order

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 """"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """"""
-    user_agent = {'User-agent': 'Mozilla/5.0'}
-    url = 'https://www.reddit.com/r'
-    reqs = requests.get('{}/{}/about.json'.format(url, subreddit),
-                        headers={'User-Agent': 'Mozilla/5.0'})
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    reqs = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     if reqs.status_code != 200:
         return 0
-    return reqs.json()["data"]['subscribers']
+    reqs = reqs.json()
+
+    return reqs["data"]["subscribers"]
