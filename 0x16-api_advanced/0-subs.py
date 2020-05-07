6048a5425fde831d50b193db0ec4c6d9137194a6
AdrianaAriza <1296@holbertonschool.com>
2020-05-07 18:28:35 -0500

    [Fix]: if 404

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import sys
-
 import requests
+
 
 def number_of_subscribers(subreddit):
     user_agent = {'User-agent': 'Mozilla/5.0'}
     url = 'https://www.reddit.com/r'
-    reqs = requests.get('{}/{}/about.json'.format(url, sys.argv[1]), headers=user_agent).json()
-    print(reqs)
-    return reqs["data"]['subscribers']
+    reqs = requests.get('{}/{}/about.json'.format(url, subreddit),
+                        headers=user_agent)
+    if reqs.status_code != 200:
+        return 0
+    return reqs.json()["data"]['subscribers']
