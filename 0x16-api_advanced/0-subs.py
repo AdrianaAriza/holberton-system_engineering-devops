827ddb91bca68d11b6552dbd7d7fc8958ea0abcd
AdrianaAriza <55102581+AdrianaAriza@users.noreply.github.com>
2020-05-07 18:47:23 -0500

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,14 @@
-# !/usr/bin/python3
-""""""
+ 
+#!/usr/bin/python3
+"number of subscribers"
 
 import requests
 
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    reqs = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-    if reqs.status_code != 200:
+    req = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    json_req = req.json()
+    if req.status_code != 200:
         return 0
-    reqs = reqs.json()
-
-    return reqs["data"]["subscribers"]
+    return(json_req["data"]["subscribers"])
