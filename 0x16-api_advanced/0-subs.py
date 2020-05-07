f19eee8e3c77e9432efa046d9b3d0407a637405a
AdrianaAriza <55102581+AdrianaAriza@users.noreply.github.com>
2020-05-07 18:41:30 -0500

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     user_agent = {'User-agent': 'Mozilla/5.0'}
     url = 'https://www.reddit.com/r'
     reqs = requests.get('{}/{}/about.json'.format(url, subreddit),
-                        headers=user_agent)
+                        headers={'User-Agent': 'Mozilla/5.0'})
     if reqs.status_code != 200:
         return 0
     return reqs.json()["data"]['subscribers']
