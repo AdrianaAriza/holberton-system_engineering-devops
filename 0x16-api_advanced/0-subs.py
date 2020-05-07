e758d9887ae80ec3889c6115a3f4020beb4b916b
AdrianaAriza <1296@holbertonschool.com>
2020-05-07 18:34:10 -0500

    [Fix]: doc

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,6 +4,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """"""
     user_agent = {'User-agent': 'Mozilla/5.0'}
     url = 'https://www.reddit.com/r'
     reqs = requests.get('{}/{}/about.json'.format(url, subreddit),
