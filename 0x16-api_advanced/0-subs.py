2dc3bd423d30b142a737fdecb6d98cd878c578c1
AdrianaAriza <1296@holbertonschool.com>
2020-05-07 19:09:05 -0500

    [Fix]: past commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    req = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-    if req.status_code != 200:
+    reqs = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    if reqs.status_code != 200:
         return 0
-    req = req.json()
-    return req["data"]["subscribers"]
+    reqs = reqs.json()
+
+    return reqs["data"]["subscribers"]
