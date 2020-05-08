722fe9cbb55a172577fd7e82f8f67001abae49b7
AdrianaAriza <1296@holbertonschool.com>
2020-05-07 19:25:53 -0500

    [FIx]

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,8 @@ import requests
 def number_of_subscribers(subreddit):
     """numbers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    req = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    req = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'},
+                       allow_redirects=False)
     if req.status_code != 200:
         return 0
     req = req.json()
