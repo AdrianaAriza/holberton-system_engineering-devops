56689dfcfcc7cc51eaba3626a6d1a7a0a1020a77
AdrianaAriza <1296@holbertonschool.com>
2020-05-07 20:56:52 -0500

    [Upload]: Task1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""subscribers"""
+import requests
+
+
+def top_ten (subreddit):
+    """numbers"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    req = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}, params={'limit': '10'})
+    if req.status_code != 200:
+        print(None)
+    req = req.json()
+    for r in req['data']['children']:
+        print(r['data']['title'])
