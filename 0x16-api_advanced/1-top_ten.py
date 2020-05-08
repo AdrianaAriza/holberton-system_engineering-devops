41eadbff3c2f3348f8621c1cbf02f87c47cd21a3
AdrianaAriza <1296@holbertonschool.com>
2020-05-07 20:59:49 -0500

    [FIx]: redirect

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """numbers"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     req = requests.get(url, headers={'User-agent': 'Mozilla/5.0'},
-                       params={'limit': '10'})
+                       params={'limit': '10'}, allow_redirects=False)
     if req.status_code != 200:
         print(None)
     req = req.json()
