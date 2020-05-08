467cc8dc9f1b4b55f23c61da613fdecffb380670
AdrianaAriza <1296@holbertonschool.com>
2020-05-07 20:57:46 -0500

    [FIx]: Style

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,10 +3,11 @@
 import requests
 
 
-def top_ten (subreddit):
+def top_ten(subreddit):
     """numbers"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    req = requests.get(url, headers={'User-agent': 'Mozilla/5.0'}, params={'limit': '10'})
+    req = requests.get(url, headers={'User-agent': 'Mozilla/5.0'},
+                       params={'limit': '10'})
     if req.status_code != 200:
         print(None)
     req = req.json()
